c68df6c4f6e30582453593d812d3a3caa326d676
Lauri Vanhatalo <lauri.vanhatalo@nokia.com>
2018-11-27 16:01:48 +0200

    Exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((prev, cur) => prev + cur.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const favoriteBlog = blogs.reduce((prev, cur) => {
+    if (prev === undefined) {
+      return cur
+    }
+    return cur.likes > prev.likes ? cur : prev
+  }, undefined)
+  return favoriteBlog === undefined ? undefined : {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }

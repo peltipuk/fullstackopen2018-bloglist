0e687787416b6e214570aa39019c58c2091ff81c
Lauri Vanhatalo <lauri.vanhatalo@nokia.com>
2018-11-27 15:03:52 +0200

    Exercise 4.2

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms', { immediate: true }))
+morgan.token('body', (req, res) => {
+  return req.body ? JSON.stringify(req.body) : '{}'
 })
-
-module.exports = Blog
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
 // MongoDB connection
 if (process.env.NODE_ENV !== 'production') {
@@ -30,25 +28,6 @@ mongoose
   .catch(err => {
     console.log(err)
   })
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  console.log('Adding blog', blog)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {

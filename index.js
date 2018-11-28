854dfe1055c0f57d49748cc6365fce787af34244
Lauri Vanhatalo <lauri.vanhatalo@nokia.com>
2018-11-28 11:56:17 +0200

    Exercise 4.8

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms', { immediate: true }))
 morgan.token('body', (req, res) => {
@@ -16,20 +18,25 @@ app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 
 // MongoDB connection
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 mongoose.set('useNewUrlParser', true)
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(config.mongoUrl)
   .then(() => {
-    console.log('connected to database', process.env.MONGODB_URI)
+    console.log('connected to database', config.mongoUrl)
   })
   .catch(err => {
     console.log(err)
   })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
+server.on('close', () => {
+  console.log('Closing mongoose connection')
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
